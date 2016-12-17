908b9795969013aed4e9dae8e11a11ffe286690d
Marco Lüthy <marco@luethy.net>
2016-12-17 15:41:21 -0800

    bunch of tooling and stuff

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/handler',
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+    ],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: '.webpack',
+    filename: 'handler.js', // this should match the first part of function handler in serverless.yml
+  },
+  externals: ['sharp', 'aws-sdk'],
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false, warnings: false }),
+  ],
+}
