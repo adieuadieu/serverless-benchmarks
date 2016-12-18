d225e5af9ae9f5b8d32c2179d04f01577c9ee9a2
Marco Lüthy <marco@luethy.net>
2016-12-17 20:28:42 -0800

    tweaks

--- src/benchmark.js
+++ src/benchmark.js
@@ -2,57 +2,73 @@ import request from 'request'
 import math from 'mathjs'
 import ProgressBar from 'progress'
 
-const limit = 100
-const url = 'https://q6fn31rhzk.execute-api.us-west-2.amazonaws.com/dev/benchmark/graphql/hello'
-const query = '{ hello(name: "Bob") }'
+const LIMIT = 10000
+const URL = 'https://q6fn31rhzk.execute-api.us-west-2.amazonaws.com/dev/benchmark/graphql/hello'
+const QUERY = '{ hello(name: "Bob") }'
 
-const startDate = Date.now()
-const progressBar = new ProgressBar(':bar :current/:total (:percent) - Elapsed :elapsed - ETA :eta', { total: limit })
-
-function makeRequestPromise () {
+function makeRequestPromise (url, query) {
   return new Promise((resolve, reject) => {
     let begin = Date.now()
+    let end
+    let delta
 
-    request({ url, qs: { query } })
-      .on('socket', () => {
-        begin = Date.now()
-      })
-      .on('response', () => {
-        const end = Date.now()
-        const delta = end - begin
-        resolve(delta)
-      })
-      .on('error', reject)
+    request({ url, qs: { query } }, (error, response, body) => {
+      if (error && response.statusCode !== 200) return reject(error)
+
+      return resolve(delta)
+    })
+    .on('socket', () => {
+      begin = Date.now()
+    })
+    .on('response', () => {
+      end = Date.now()
+      delta = end - begin
+    })
+    .on('error', reject)
   })
 }
 
-async function makeRequests () {
+async function makeRequests (url, query, limit, progressBar) {
   const results = []
 
   for (let i = 0; i < limit; i++) {
-    results[i] = await makeRequestPromise(i)
+    try {
+      results[i] = await makeRequestPromise(url, query)
+    } catch (error) {
+      console.log('request error', error)
+    }
+
     progressBar.tick()
   }
 
   return results
 }
 
-async function benchmark () {
-  const results = await makeRequests()
+async function benchmark (url, query, limit) {
+  const progressBar = new ProgressBar(':bar :current/:total (:percent) - Elapsed :elapsed - ETA :eta', { total: limit })
 
-  console.log('results', results)
+  const startDate = Date.now()
+  const results = await makeRequests(url, query, limit, progressBar)
+  const completionDate = Date.now()
+
+  const totalRequests = results.length
+  const totalDuration = Date.now() - startDate
 
   const min = Math.min(...results)
   const max = Math.max(...results)
   const mean = math.mean(...results)
 
-  console.log('=== Results ===')
-  console.log('Date', Date().toLocaleString())
-  console.log(`Duration:\t${Date.now() - startDate}ms`)
-  console.log(`Requests:\t${results.length}ms`)
-  console.log(`Min:\t\t${min}ms`)
-  console.log(`Max:\t\t${max}ms`)
-  console.log(`Mean:\t\t${mean}`)
+  const data = [Date(completionDate).toLocaleString(), totalRequests, totalDuration, min, max, mean]
+
+  console.log('———— Results ————')
+  console.log('Completion Date', Date(completionDate).toLocaleString())
+  console.log(`Total Duration:\t\t${totalDuration}ms`)
+  console.log(`Requests made:\t\t${totalRequests}`)
+  console.log(`Duration Min:\t\t${min}ms`)
+  console.log(`Duration Max:\t\t${max}ms`)
+  console.log(`Duration Mean:\t\t${mean}`)
+
+  return data
 }
 
-benchmark()
+benchmark(URL, QUERY, LIMIT)
